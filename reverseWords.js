def4ae15f30d890658ba1f1b6b7d6a2345806a35
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-07-31 18:01:27 -0700

    reverse sentence

---
+++ reverseWords.js
@@ -0,0 +1,10 @@
+function reverseWords(){
+    const givenSentence = document.getElementById("inputReverse").value ;
+    document.getElementById("reverse_words").innerHTML=reverse_Words(givenSentence);
+}
+function reverse_Words(sentence){
+    const array = sentence.split(" ");
+    const reverseArray = array.reverse();
+    const reverseSentence = reverseArray.join();
+    return reverseSentence;
+}
