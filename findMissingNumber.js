e8372ea82b04133d091a8fdaf120dc05e2ca19ad
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-08-01 13:12:50 -0700

    //

---
+++ findMissingNumber.js
@@ -0,0 +1,16 @@
+function findMissingNumberGauss(nums) {
+  const n = nums.length + 1;
+  const totalSum = (n * (n + 1)) / 2;
+  const actualSum = nums.reduce((acc, num) => acc + num, 0);
+  const missingNumber = totalSum - actualSum;
+  return missingNumber;
+}
+function findMissingNum(){
+  const inputArrayString = document.getElementById("inputArray").value;
+    const inputArray = inputArrayString
+      .split(",") // Split the input string by commas to get individual numbers
+      .map((num) => parseInt(num.trim(), 10)); // Convert each string number to an actual number
+
+    const missingNumberFromArray = logic(inputArray);
+    document.getElementById("output").innerHTML = missingNumberFromArray;
+}
