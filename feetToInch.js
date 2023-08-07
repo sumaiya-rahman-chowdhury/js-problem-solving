7a9a7a71a807355e12dfee75ba4b0696aae88010
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-08-06 18:55:02 -0700

    feetToInch converter / vice verse

---
+++ feetToInch.js
@@ -0,0 +1,20 @@
+
+// Inch to Feet converter 
+
+/*  function inchToFeet(inch){
+    let result = (1/12)*inch ;
+    return result ;
+}
+let calculate  = inchToFeet(40);
+console.log(calculate) 
+ */
+
+// Feet to inch
+
+function feetToInch(feet){
+    const result = (12*feet);
+    return result ;
+}
+
+let calculate = feetToInch(5);
+console.log(calculate);
