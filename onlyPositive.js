d66f707c9315abc6e10f3a57aee748d692ab53b8
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-08-06 20:38:51 -0700

    comparison of string + postive integer

---
+++ onlyPositive.js
@@ -0,0 +1,21 @@
+function onlyPositive(array){
+    if(!Array.isArray(array)){
+        return "Provide me an Array "
+    }
+    let value = []
+    for(let i=0; i<array.length; i++){
+       
+        if(array[i]>=0){
+            value.push(array[i])
+        }
+        else{
+            break ;
+        }
+    }
+    return value;
+}
+
+
+const mixedArray = [5, 0, 12, -8, 15, -10, 20, -3]; 
+const result = onlyPositive(mixedArray);
+console.log(result.join(" "));
