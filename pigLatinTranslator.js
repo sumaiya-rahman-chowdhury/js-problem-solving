13d8d92dfb8ac55174038f51cd19b54fd75fdf51
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-08-07 09:21:54 -0700

    pig translator

--- pigLatinTranslator.js
+++ pigLatinTranslator.js
@@ -1,5 +1,9 @@
+/* // Problem 13: Pig Latin Translator
+
+Write a function that takes a word as input and translates it into Pig Latin. In Pig Latin, you move the first letter of the word to the end and add "ay". For example, "hello" becomes "ellohay". */
+
 function pigLatinTranslator(word){
-    let newWord = word.substring(1)+slice(0,word.length-1);
+    let newWord = word.substring(1)+ word.slice(0,1) +"ey";
    
     return newWord ;
 }
